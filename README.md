ebe968df68567272aae6a0cefa35f027a9618f1b
AmirMohammadi <amirmm@web.de>
2023-03-09 15:31:25 +0100

    test.amir

---
+++ README.md
@@ -0,0 +1,5 @@
+08.03.2023
+    Ansammlung von ideen und Hilfmittel 
+
+09.03.2023
+    
---
+++ backend/checkInput.js
@@ -0,0 +1,12 @@
+/*
+    Diese Datei ist dafür da um die Eingabefelder zu Ueberpruefen 
+    ob die Eingaben auch nach Anforderung eingegeben wurden ist 
+
+    Bspl.: 
+    bei Vor-Nachname keine Zahlen 
+    bei Email muss das " @ " zeichen mit dabei sein um 
+    bei PLZ max 5 Zahlen 
+    IBAN muss mit 2 Buchstaben beginnen 
+    und und und  
+    neue Zeile 
+*/
