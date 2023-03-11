9aa748162a50d7ed62a9ef1e9438ea59326e9a51
Abdurrahman Yildirim <abdurrahman.yildirim@student.techstarter.de>
2023-03-11 13:49:34 +0100

    bischen am Projekt gearbeitet

--- backend/saveCSV.js
+++ backend/saveCSV.js
@@ -1,45 +1,52 @@
-/*
-    Diese Datei ist dafür da um die CSV Datei abzuspeichern 
-    
+const { write } = require('fs');
 
-*/
+// Bibliothek csv-writer
+const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 
-const form = document.querySelector('#myForm');
-form.addEventListener('submit', handleSubmit);
+// Pfad definieren für die CSV Datei
+const csvPath = './data.csv';
 
-function handleSubmit(e) {
-    e.preventDefault();
-    const personalnr = document.querySelector('#personalnummer').value;
-    const anrede = document.querySelector('#anrede').value;
-    const vorname = document.querySelector('#vorname').value;
-    const nachname = document.querySelector('#nachname').value;
-    const bday = document.querySelector('#geburtsdatum').value;
-    const staatsangehoerigkeit = document.querySelector('#staatsangehoerigkeit').value;
-    const geschlecht = document.querySelector('#geschlecht').value;
-    const email = document.querySelector('#email').value;
-    const telefon = document.querySelector('#telefon').value;
-    const strasse = document.querySelector('#strasse').value;
-    const plz = document.querySelector('#plz').value;
-    const wohnort = document.querySelector('#wohnort').value;
-    const kontoinhaber = document.querySelector('#kontoinhaber').value;
-    const iban = document.querySelector('#iban').value;
 
-      
-    const data = { personalnr, anrede, vorname };
-    const csv = convertToCSV(data); saveToCSV(csv);
+// Funktion zum Schreiben der CSV Datei
+function writeToCsvFile() {
+
+// Auf den Inhalt der Eingabefelder mit get.ElementById zugreifen  
+const anrede = document.getElementById('anrede').value;
+const vorname = document.querySelector('vorname').value;
+const nachname = document.querySelector('nachname').value;
+const bday = document.querySelector('geburtsdatum').value;
+const geschlecht = document.querySelector('geschlecht').value;
+const staatsangehoerigkeit = document.querySelector('staatsangehoerigkeit').value;
+const email = document.querySelector('email').value;
+const telefon = document.querySelector('telefon').value;
+const strasse = document.querySelector('strasse').value;
+const plz = document.querySelector('plz').value;
+const wohnort = document.querySelector('wohnort').value;
+const bundesland = document.querySelector('bundesland').value;
+const kontoinhaber = document.querySelector('kontoinhaber').value;
+const iban = document.querySelector('iban').value;
+const kreditinstitut = document.querySelector('kreditinstitut').value;
+const steuerid = document.querySelector('steueridentifikationsnummer').value;
+const steuerklasse = document.querySelector('steuerklasse').value;
+const krankenkasse = document.querySelector('krankenkasse').value;
+const versicherungsnummer = document.querySelector('versicherungsnummer').value;
+const beginn = document.querySelector('beginn').value;
+const berufsbezeichnung = document.querySelector('berufsbezeichnung').value;
+const abteilung = document.querySelector('abteilung').value;
+
+
+
+// Schreiben in die CSV Datei
+const csvWriter = createCsvWriter({
+    header: ['Andrede', 'Vorname'],
+    path: csvPath,
+    append: true
+});
+
+csvWriter.writeRecords([
+    [anrede, vorname]
+]);
+
 }
 
-function convertonToCSV(data) {
-    const rows = [Object.keys(data), Object.values(data)];
-    return rows.map(row => row.join(',')).join('\n');
-}
-
-function saveToCSV(csv) {
-    const filename = 'data.csv';
-    const blob = new Blob([csv], { type: 'text/csv' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url; a.download = filename; a.click();
-}
-
-
+document.getElementById('savedata').addEventListener('click', writeToCsvFile)
