eae38317311a61f1a056cba4471c9ae33f81cca0
Abdurrahman Yildirim <abdurrahman.yildirim@student.techstarter.de>
2023-03-13 11:35:52 +0100

    saveCSV file complete read and save

--- backend/saveCSV.js
+++ backend/saveCSV.js
@@ -1,52 +1,52 @@
-const { write } = require('fs');
-
-// Bibliothek csv-writer
-const createCsvWriter = require('csv-writer').createArrayCsvWriter;
-
-// Pfad definieren für die CSV Datei
-const csvPath = './data.csv';
-
 
 // Funktion zum Schreiben der CSV Datei
-function writeToCsvFile() {
+function saveToCSV(){
 
 // Auf den Inhalt der Eingabefelder mit get.ElementById zugreifen  
 const anrede = document.getElementById('anrede').value;
-const vorname = document.querySelector('vorname').value;
-const nachname = document.querySelector('nachname').value;
-const bday = document.querySelector('geburtsdatum').value;
-const geschlecht = document.querySelector('geschlecht').value;
-const staatsangehoerigkeit = document.querySelector('staatsangehoerigkeit').value;
-const email = document.querySelector('email').value;
-const telefon = document.querySelector('telefon').value;
-const strasse = document.querySelector('strasse').value;
-const plz = document.querySelector('plz').value;
-const wohnort = document.querySelector('wohnort').value;
-const bundesland = document.querySelector('bundesland').value;
-const kontoinhaber = document.querySelector('kontoinhaber').value;
-const iban = document.querySelector('iban').value;
-const kreditinstitut = document.querySelector('kreditinstitut').value;
-const steuerid = document.querySelector('steueridentifikationsnummer').value;
-const steuerklasse = document.querySelector('steuerklasse').value;
-const krankenkasse = document.querySelector('krankenkasse').value;
-const versicherungsnummer = document.querySelector('versicherungsnummer').value;
-const beginn = document.querySelector('beginn').value;
-const berufsbezeichnung = document.querySelector('berufsbezeichnung').value;
-const abteilung = document.querySelector('abteilung').value;
+const vorname = document.getElementById('vorname').value;
+const nachname = document.getElementById('nachname').value;
+const bday = document.getElementById('geburtsdatum').value;
+const geschlecht = document.getElementById('geschlecht').value;
+const staatsangehoerigkeit = document.getElementById('staatsangehoerigkeit').value;
+const email = document.getElementById('email');
+const telefon = document.getElementById('telefon').value;
+const strasse = document.getElementById('strasse').value;
+const plz = document.getElementById('plz').value;
+const wohnort = document.getElementById('wohnort').value;
+const bundesland = document.getElementById('bundesland').value;
+const kontoinhaber = document.getElementById('kontoinhaber').value;
+const iban = document.getElementById('iban').value;
+const kreditinstitut = document.getElementById('kreditinstitut').value;
+const steuerid = document.getElementById('steueridentifikationsnummer').value;
+const steuerklasse = document.getElementById('steuerklasse').value;
+const krankenkasse = document.getElementById('krankenkasse').value;
+const versicherungsnummer = document.getElementById('versicherungsnummer').value;
+const beginn = document.getElementById('beginn').value;
+const berufsbezeichnung = document.getElementById('berufsbezeichnung').value;
+const abteilung = document.getElementById('abteilung').value;
 
 
+/////////////
 
-// Schreiben in die CSV Datei
-const csvWriter = createCsvWriter({
-    header: ['Andrede', 'Vorname'],
-    path: csvPath,
-    append: true
-});
+const csvContent = "Anrede,Vorname,Nachname,Geburtsdatum,Geschlecht,Staatsangehoerigkeit,E-mail,Telefon,Straße,PLZ,Wohnort,Bundesland,Kontoinhaber,Iban,Kreditinstitut,SteuerID,Steuerklasse,Krankenkasse,Verischerungsnummer,beginn,Berufsbezeichnung,Abteilung\n" + // Die erste Zeile enthält die Spaltenüberschriften
+                    `${anrede},${vorname},${nachname},${bday},
+                     ${geschlecht},${staatsangehoerigkeit},${email},${telefon},
+                     ${strasse},${plz},${wohnort},${bundesland},
+                     ${kontoinhaber},${iban},${kreditinstitut},${steuerid},
+                     ${steuerklasse},${krankenkasse},${versicherungsnummer},${beginn},
+                     ${berufsbezeichnung},${abteilung},`;
+/*                    
+var daten = [anrede,vorname,nachname,bday,geschlecht,
+            staatsangehoerigkeit,email,telefon,strasse,plz,
+            wohnort,bundesland,kontoinhaber,iban,kreditinstitut,steuerid,steuerklasse,krankenkasse,versicherungsnummer,
+            beginn,berufsbezeichnung,abteilung].join(",") + "\n";
+*/
+var a = document.createElement("a");
+var csvFile = new Blob([csvContent], {type: 'text/csv;charset=utf-8;'});
 
-csvWriter.writeRecords([
-    [anrede, vorname]
-]);
+a.href = URL.createObjectURL(csvFile);
+a.download = "daten.csv";
+a.click();
 
 }
-
-document.getElementById('savedata').addEventListener('click', writeToCsvFile)
