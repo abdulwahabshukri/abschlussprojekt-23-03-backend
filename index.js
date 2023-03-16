58dd186e03d6f42df06344581c196b4fac4c724f
Serdar Sadikoglu <serdar.sadikoglu@student.techstarter.de>
2023-03-16 11:50:39 +0100

    CSV Datei funktionert

--- index.js
+++ index.js
@@ -1,48 +1,85 @@
-// Initialisation d'une variable globale pour stocker les données
-let csvData = "Personalnummer,Anrede,Vorname,Nachname,Geburtsdatum,Geschlecht,Staatsangehoerigkeit,E-mail,Telefon,Straße,PLZ,Wohnort,Bundesland,Kontoinhaber,Iban,Kreditinstitut,SteuerID,Steuerklasse,Krankenkasse,Verischerungsnummer,beginn,Berufsbezeichnung,Abteilung\n";
+function enregistrerDonnees(event) {
+  event.preventDefault(); // Formularübermittlung verhindern
 
-// Fonction pour ajouter les données à la variable csvData
-function appendToCSV() {
-  const personalnr = document.getElementById('personalnummer').value;
-  const anrede = document.getElementById('anrede').value;
-  const vorname = document.getElementById('vorname').value;
-  const nachname = document.getElementById('nachname').value;
-  const bday = document.getElementById('geburtsdatum').value;
-  const geschlecht = document.getElementById('geschlecht').value;
-  const staatsangehoerigkeit = document.getElementById('staatsangehoerigkeit').value;
-  const email = document.getElementById('email').value;
-  const telefon = document.getElementById('telefon').value;
-  const strasse = document.getElementById('strasse').value;
-  const plz = document.getElementById('plz').value;
-  const wohnort = document.getElementById('wohnort').value;
-  const bundesland = document.getElementById('bundesland').value;
-  const kontoinhaber = document.getElementById('kontoinhaber').value;
-  const iban = document.getElementById('iban').value;
-  const kreditinstitut = document.getElementById('kreditinstitut').value;
-  const steuerid = document.getElementById('steueridentifikationsnummer').value;
-  const steuerklasse = document.getElementById('steuerklasse').value;
-  const krankenkasse = document.getElementById('krankenkasse').value;
-  const versicherungsnummer = document.getElementById('versicherungsnummer').value;
-  const beginn = document.getElementById('beginn').value;
-  const berufsbezeichnung = document.getElementById('berufsbezeichnung').value;
-  const abteilung = document.getElementById('abteilung').value;
+  // Abrufen von Formularfeldwerten
+  const personalnummer = document.getElementById("personalnummer").value; 
+  const anrede = document.getElementById("anrede").value;
+  const vorname = document.getElementById("vorname").value;
+  const nachname = document.getElementById("nachname").value;
+  const geburtsdatum = document.getElementById("geburtsdatum").value;
+  const geschlecht = document.getElementById("geschlecht").value;
+  const staatsangehoerigkeit = document.getElementById("staatsangehoerigkeit").value; 
+  const email = document.getElementById("email").value;      
+  const telefon = document.getElementById("telefon").value;
+  const strasse = document.getElementById("strasse").value;
+  const plz = document.getElementById("plz").value;
+  const wohnort = document.getElementById("wohnort").value;  
+  const bundesland = document.getElementById("bundesland").value;
+  const kontoinhaber = document.getElementById("kontoinhaber").value;
+  const iban = document.getElementById("iban").value;
+  const kreditinstitut = document.getElementById("kreditinstitut").value;
+  const steueridentifikationsnummer = document.getElementById("steueridentifikationsnummer").value;
+  const steuerklasse = document.getElementById("steuerklasse").value;
+  const krankenkasse = document.getElementById("krankenkasse").value;
+  const versicherungsnummer = document.getElementById("versicherungsnummer").value;
+  const beginn = document.getElementById("beginn").value;
+  const berufsbezeichnung = document.getElementById("berufsbezeichnung").value;
+  const abteilung = document.getElementById("abteilung").value;
 
-  const csvContent = `${personalnr},${anrede},${vorname},${nachname},${bday},${geschlecht},${staatsangehoerigkeit},${email},${telefon},${strasse},${plz},${wohnort},${bundesland},${kontoinhaber},${iban},${kreditinstitut},${steuerid},${steuerklasse},${krankenkasse},${versicherungsnummer},${beginn},${berufsbezeichnung},${abteilung}\n`;
 
-  csvData += csvContent;
-}
+ /* // Überprüfung von Formularfeldern
+  if (personalnummer === '' || anrede === '' || vorname === ''  ||  nachname === '' || geburtsdatum === '' || geschlecht === '' || staatsangehoerigkeit === ''
+      || email === '' || telefon === '' || strasse === '' || plz === '' || wohnort === '' || bundesland === '' || kontoinhaber === '' || iban === '' || kreditinstitut === ''
+      || steueridentifikationsnummer === '' || steuerklasse === '' || krankenkasse === '' || versicherungsnummer === '' || beginn === ''
+      || berufsbezeichnung === '' || abteilung === '') {
+      alert('Bitte füllen Sie alle Felder des Formulars aus');
+      return;
+  }*/
 
-// Fonction pour télécharger le fichier CSV
-function saveToCSV() {
-  appendToCSV();
+  // Erstellen eines Objekts mit Formulardaten
+  const donnees = {
+      personalnummer: personalnummer,
+      anrede: anrede,
+      vorname: vorname,
+      nachname: nachname,
+      geburtsdatum: geburtsdatum,
+      geschlecht: geschlecht,
+      staatsangehoerigkeit: staatsangehoerigkeit,
+      email: email,                        
+      telefon: telefon,
+      strasse: strasse,
+      plz: plz,
+      wohnort: wohnort,
+      bundesland: bundesland,
+      kontoinhaber: kontoinhaber,
+      iban: iban,
+      kreditinstitut: kreditinstitut,
+      steueridentifikationsnummer: steueridentifikationsnummer,
+      steuerklasse: steuerklasse,
+      krankenkasse: krankenkasse,
+      versicherungsnummer: versicherungsnummer,
+      beginn: beginn,
+      berufsbezeichnung: berufsbezeichnung,
+      abteilung: abteilung
+  };
+  console.log(donnees);
+  
+  // Senden von Daten an den Node.js-Server mit Abruf
+  fetch('/enregistrer-donnees', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',              
+      },
+      body: JSON.stringify(donnees)
 
-  const blob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
-  const url = URL.createObjectURL(blob);
-  const a = document.createElement("a");
-
-  a.href = url;
-  a.download = "daten.csv";
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+  })
+      .then(response => response.json())
+      .then(data => {
+          if (data.success) {
+              alert('Daten erfolgreich gespeichert !');
+          } else {
+              alert('Beim Speichern der Daten ist ein Fehler aufgetreten.');
+          }
+      })
+      .catch(error => console.error(error));
 }
