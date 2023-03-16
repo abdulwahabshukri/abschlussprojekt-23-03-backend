60fbe986dc3f01e69f592df6b7c0c76e1c061006
Serdar Sadikoglu <serdar.sadikoglu@student.techstarter.de>
2023-03-16 15:29:06 +0100

    button für CSV-Datei herunterladen fertig

--- server.js
+++ server.js
@@ -3,19 +3,32 @@ const app = express();
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// CSV-Datei herunterladen
+app.get('/telecharger-donnees', function(req, res) {
+    res.download('donnees.csv', function(err) {
+      if (err) {
+        console.log(err);
+        res.status(404).send('Datei nicht gefunden');
+      }
+    });
+  });
+
 app.post('/enregistrer-donnees', (req, res) => {
     const donnees = req.body;
     console.log(donnees);
-    console.log(req.body);
+    
 
     if (!donnees) {
         console.error('Die Daten sind leer.');
@@ -23,11 +36,6 @@ app.post('/enregistrer-donnees', (req, res) => {
         return;
     }
 
-    /*if (!donnees.personalnummer || !donnees.anrede ||  !donnees.vorname || !donnees.nachname || !donnees.geburtsdatum || !donnees.geschlecht || !donnees.staatsangehoerigkeit || !donnees.email || !donnees.telefon || !donnees.strasse || !donnees.plz || !donnees.wohnort || !donnees.bundesland || !donnees.kontoinhaber || !donnees.iban || !donnees.kreditinstitut || !donnees.steueridentifikationsnummer || !donnees.steuerklasse || !donnees.krankenkasse || !donnees.versicherungsnummer || !donnees.beginn || !donnees.berufsbezeichnung || !donnees.abteilung) {
-        console.error('Die Daten sind unvollständig.');
-        res.json({ success: false });
-        return;
-    }*/
 
     fs.stat('donnees.csv', function (err, stat) {
         if (err == null) {
